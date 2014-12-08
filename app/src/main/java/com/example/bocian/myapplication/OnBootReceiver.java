d10354ac4cddf08b318fab0f6bf60ef2a832dc6c
mikebocian <michbocian@gmail.com>
2014-12-08 17:30:14 +0100

    k

--- app/src/main/java/com/example/bocian/myapplication/OnBootReceiver.java
+++ app/src/main/java/com/example/bocian/myapplication/OnBootReceiver.java
@@ -1,27 +1,19 @@
 package com.example.bocian.myapplication;
 
-import android.app.AlarmManager;
-import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.os.SystemClock;
 import android.util.Log;
 
 public class OnBootReceiver extends BroadcastReceiver {
-    private static final int PERIOD=5000;  // 5 minutes
+    private static final int PERIOD = 5000;  // 5 minutes
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        Log.d("MyApp","On boot receiver");
-        AlarmManager mgr=(AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
-        Intent i=new Intent(context, OnAlarmRecceiver.class);
-        PendingIntent pi=PendingIntent.getBroadcast(context, 0,
-                i, 0);
+        Log.d("MyApp", "On boot receiver");
+        Intent i = new Intent(context, OnAlarmRecceiver.class);
 
-        mgr.setRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,
-                SystemClock.elapsedRealtime()+5000,
-                PERIOD,
-                pi);
+        TimerManager.addIntentToAlarmMgr(i, context, 5000);
     }
 }
+
--- app/src/main/java/com/example/bocian/myapplication/TimerManager.java
+++ app/src/main/java/com/example/bocian/myapplication/TimerManager.java
@@ -1,7 +1,38 @@
 package com.example.bocian.myapplication;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.SystemClock;
+
 /**
  * Created by bocian on 08.12.14.
  */
-public class TimerManager {
+public final class TimerManager {
+
+    private TimerManager() {
+        throw new AssertionError();
+    }
+
+    public static final void addIntentToAlarmMgr(Intent intent, Context ctx, long period) {
+        AlarmManager mgr = (AlarmManager) ctx.getSystemService(Context.ALARM_SERVICE);
+
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(ctx, 0, intent, 0);
+
+        mgr.setRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                SystemClock.elapsedRealtime() + 5000,
+                period,
+                pendingIntent);
+    }
+
+    public static final void removeIntentFromAlarmMgr(Intent intent, Context ctx, long period) {
+        AlarmManager mgr = (AlarmManager) ctx.getSystemService(Context.ALARM_SERVICE);
+
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(ctx, 0, intent, 0);
+
+        mgr.cancel(pendingIntent);
+
+    }
+
 }
